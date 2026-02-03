33a935d683caed43161de051b5706c8c2870b4f0
ukir-rrrr <aorikuaoriku@icloud.com>
2026-02-03 10:01:53 +0900

    typography導入

--- app/(marketing)/blog/[slug]/page.tsx
+++ app/(marketing)/blog/[slug]/page.tsx
@@ -1,14 +1,39 @@
 import { allPosts } from "@/.contentlayer/generated";
 import Mdx from "@/components/mdx-component";
 import { buttonVariants } from "@/components/ui/button";
+import { siteconfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
+import { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { describe } from "node:test";
 
 async function getPostFromSlug(slug: string) {
   const post = allPosts.find((post) => post.slugAsParams === slug);
   return post;
+}
+
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+  const page = await getPostFromSlug(params.slug);
+
+  if (!page) {
+    return {};
+  }
+
+  return {
+    title: page.title,
+    description: page.description,
+    openGraph: {
+      type: "article",
+      locale: "ja",
+      url: siteconfig.url,
+      title: siteconfig.name,
+      description: siteconfig.description,
+      siteName: siteconfig.name,
+      images: [],
+    },
+  }
 }
 
 
@@ -36,7 +61,7 @@ export default async function PostPage({ params }: { params: Promise<{ slug: str
           className="my-8 border rounded-md bg-muted"
         />
       )}
-      <Mdx code={post.body.code}/>
+      <Mdx code={post.body.code} />
       <hr className="mt-12" />
       <div className="py-6 text-center lg:py-10">
         <Link href={"blog"} className={cn(buttonVariants({ variant: "ghost" }))}>全ての記事を見る</Link>
--- components/mdx-component.tsx
+++ components/mdx-component.tsx
@@ -13,7 +13,7 @@ export default function Mdx({code}: { code: string }) {
   const Component = useMDXComponent(code);
 
   return (
-    <div>
+    <div className="prose !max-w-full">
       <Component components={components}/>
     </div>
   );
